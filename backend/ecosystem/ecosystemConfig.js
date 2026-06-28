27c4715fc38e940605ef6622e425ff41d1e3963a
Jose Antonio <xantalal@gmail.com>
2026-06-28 17:56:13 +0200

    Registrar configuracion del ecosistema XANTALAL

---
+++ backend/ecosystem/ecosystemConfig.js
@@ -0,0 +1,16 @@
+const ecosystemConfig = {
+  ecosystemName: "XANTALAL",
+  ecosystemRoot: "C:\\Users\\janta\\OneDrive\\Documentos\\XANTALAL",
+  governanceFolder: "C:\\Users\\janta\\OneDrive\\Documentos\\XANTALAL\\00_GOVERNANCE",
+  governanceFiles: [
+    "README.md",
+    "00_GOVERNANCE/MANUAL_DE_GOBIERNO_XANTALAL.md",
+    "00_GOVERNANCE/DECISIONES_APROBADAS.md",
+    "00_GOVERNANCE/ORGANIGRAMA.md",
+    "00_GOVERNANCE/ROADMAP_GENERAL.md",
+    "00_GOVERNANCE/AGENTES.md",
+    "00_GOVERNANCE/ESTADO_GLOBAL.md",
+  ],
+};
+
+module.exports = ecosystemConfig;
