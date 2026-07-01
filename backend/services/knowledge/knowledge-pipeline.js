2e33d4cbb290aaf59097588049c3f05dd8d84fec
Jose Antonio <xantalal@gmail.com>
2026-07-01 19:19:22 +0200

    Integrar Universal Knowledge Curator en Knowledge Pipeline

--- backend/services/knowledge/knowledge-pipeline.js
+++ backend/services/knowledge/knowledge-pipeline.js
@@ -5,6 +5,7 @@ const { buildDocumentCatalog } = require('./document-catalog');
 const { discoverDocuments } = require('./document-discovery');
 const { locateDocuments } = require('./document-locator');
 const { buildKnowledgeCache } = require('./knowledge-cache-registry');
+const { curateDocument } = require('./universal-knowledge-curator');
 
 function runKnowledgePipeline(asset) {
   const folders = discoverTopLevelFolders();
@@ -16,18 +17,24 @@ function runKnowledgePipeline(asset) {
       folder: null,
       catalog: null,
       cache: [],
+      knowledgeObjects: [],
     };
   }
 
   const documentDiscovery = discoverDocuments(documentLocation.folder);
   const catalog = buildDocumentCatalog(documentDiscovery);
   const cache = buildKnowledgeCache(documentDiscovery.documents);
+  const knowledgeObjects = documentDiscovery.documents
+    .map((document) => curateDocument(document))
+    .filter((curation) => curation.supported)
+    .map((curation) => curation.knowledgeObject);
 
   return {
     asset,
     folder: documentLocation.folder,
     catalog,
     cache,
+    knowledgeObjects,
   };
 }
 
